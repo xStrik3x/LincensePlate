eb01cb3c1a0455c2311129694b4685ee5f101963
student <student@rba7f403-009.local>
2018-09-29 13:39:47 +0700

    start firebase

---
+++ lincenseplate/lincenseplate/FillViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FillViewController.swift
+//  lincenseplate
+//
+//  Created by student on 9/29/18.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class FillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

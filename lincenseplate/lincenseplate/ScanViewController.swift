05068decc49d487e38249362031c80da005f4686
student <student@rba7f403-009.local>
2018-10-03 14:37:18 +0700

    ScanController

---
+++ lincenseplate/lincenseplate/ScanViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScanViewController.swift
+//  lincenseplate
+//
+//  Created by student on 10/3/18.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class ScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

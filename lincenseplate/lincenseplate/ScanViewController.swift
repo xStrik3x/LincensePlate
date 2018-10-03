1ba4cfa58c9e10e5e9d889c12bc92042d15cb9c6
student <student@rba7f403-009.local>
2018-10-03 16:09:45 +0700

    image Picker

--- lincenseplate/lincenseplate/ScanViewController.swift
+++ lincenseplate/lincenseplate/ScanViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 import SwiftOCR
 
-class ScanViewController: UIViewController {
 
+class ScanViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBAction func chooseBtn(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.allowsEditing = false
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        img.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+//        let swiftOCRIns = SwiftOCR()
+//
+//        imagePicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+//        swiftOCRIns.recognize(img.image!) { recognizedString in
+//            print(recognizedString)
+//        }
     }
     
 

7ad1b6f9ea01d8f9669df6cb04c28a93a5446f46
comart <comart@rba1f403-008.local>
2018-11-09 14:37:10 +0700

    แก้ไข

--- lincenseplate/lincenseplate/FillViewController.swift
+++ lincenseplate/lincenseplate/FillViewController.swift
@@ -1,69 +1,113 @@
-//
-//  FillViewController.swift
-//  lincenseplate
-//
-//  Created by student on 9/29/18.
-//  Copyright © 2018 student. All rights reserved.
-//
 
-import UIKit
-import Firebase
-import FirebaseFirestore
 
-class FillViewController: UIViewController, UITextFieldDelegate {
+    //
+    //  ScanViewController.swift
+    //  lincenseplate
+    //
+    //  Created by student on 10/3/18.
+    //  Copyright © 2018 student. All rights reserved.
+    //
     
-    @IBOutlet weak var fillTField: UITextField!
-    @IBAction func fillBtn(_ sender: Any) {
-        if fillTField.text != ""{
-            guard let lp = fillTField.text else { return }
-            let typeRef = Firestore.firestore().collection("รถยนต์")
-            let docRef = typeRef.document(lp)
-            docRef.getDocument{ (document, err) in
-                if let document = document {
-                    
-                    if document.exists{
-                        let retriVC = self.storyboard?.instantiateViewController(withIdentifier: "retri") as! RetrieveViewController
-                        retriVC.data = lp
-                        DispatchQueue.main.async (execute: {
-                            self.present(retriVC, animated: true, completion: nil)
-                        })
-                    } else {
-                        let alert = UIAlertController(title: "ไม่มีเลขทะเบียนในระบบ", message: nil, preferredStyle: .alert)
-                        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
-                        alert.addAction(okButton)
-                        self.present(alert,animated: true,completion: nil)
+    import UIKit
+    import SwiftOCR
+    import Firebase
+    import FirebaseFirestore
+    
+    class FillViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        
+        @IBOutlet weak var img: UIImageView!
+        
+        @IBOutlet weak var plate: UITextField!
+        @IBAction func OCR(_ sender: Any) {
+            
+            if (img.image != nil){
+                let swiftOCRIns = SwiftOCR()
+                swiftOCRIns.recognize(img.image!) { recognizedString in
+                    DispatchQueue.main.async {
+                        self.plate.text = recognizedString
                     }
-                        
+                    print(recognizedString)
+                    
                 }
             }
-        } else {
-            let alert = UIAlertController(title: "โปรดกรอกเลขทะเบียนรถ", message: nil, preferredStyle: .alert)
-            let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alert.addAction(okButton)
-            self.present(alert,animated: true,completion: nil)
+            else{
+                let alert = UIAlertController(title: "โปรดเลือกรูปหรือถ่ายรูป", message: nil, preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert,animated: true,completion: nil)
+                
+            }
+            
         }
         
         
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        @IBAction func chooseBtn(_ sender: Any) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
         
-        fillTField.delegate = self
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+            img.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Do any additional setup after loading the view.
+            
+        }
+        
+        @IBAction func Okbtn(_ sender: Any) {
+            if plate.text != ""{
+                guard let lp = plate.text else { return }
+                let typeRef = Firestore.firestore().collection("รถยนต์")
+                let docRef = typeRef.document(lp)
+                docRef.getDocument{ (document, err) in
+                    if let document = document {
+                        
+                        if document.exists{
+                            let retriVC = self.storyboard?.instantiateViewController(withIdentifier: "retri") as! RetrieveViewController
+                            retriVC.data = lp
+                            DispatchQueue.main.async (execute: {
+                                self.present(retriVC, animated: true, completion: nil)
+                            })
+                        } else {
+                            let alert = UIAlertController(title: "ไม่มีเลขทะเบียนในระบบ", message: nil, preferredStyle: .alert)
+                            let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                            alert.addAction(okButton)
+                            self.present(alert,animated: true,completion: nil)
+                        }
+                        
+                    }
+                }
+            } else {
+                let alert = UIAlertController(title: "โปรดกรอกเลขทะเบียนรถ", message: nil, preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert,animated: true,completion: nil)
+            }
+        }
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            self.view.endEditing(true)
+            return false
+        }
         
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.endEditing(true)
-        return false
-    }
+
+
     
 
-}
+
--- lincenseplate/lincenseplate/ScanViewController.swift
+++ lincenseplate/lincenseplate/ScanViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 import SwiftOCR
-
+import Firebase
+import FirebaseFirestore
 
 class ScanViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -53,6 +54,8 @@ class ScanViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         self.dismiss(animated: true, completion: nil)
     }
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -60,5 +63,44 @@ class ScanViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         
     }
     
-
+    @IBAction func Okbtn(_ sender: Any) {
+        if plate.text != ""{
+            guard let lp = plate.text else { return }
+            let typeRef = Firestore.firestore().collection("รถยนต์")
+            let docRef = typeRef.document(lp)
+            docRef.getDocument{ (document, err) in
+                if let document = document {
+                    
+                    if document.exists{
+                        let retriVC = self.storyboard?.instantiateViewController(withIdentifier: "retri") as! RetrieveViewController
+                        retriVC.data = lp
+                        DispatchQueue.main.async (execute: {
+                            self.present(retriVC, animated: true, completion: nil)
+                        })
+                    } else {
+                        let alert = UIAlertController(title: "ไม่มีเลขทะเบียนในระบบ", message: nil, preferredStyle: .alert)
+                        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                        alert.addAction(okButton)
+                        self.present(alert,animated: true,completion: nil)
+                    }
+                    
+                }
+            }
+        } else {
+            let alert = UIAlertController(title: "โปรดกรอกเลขทะเบียนรถ", message: nil, preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert,animated: true,completion: nil)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
 }
